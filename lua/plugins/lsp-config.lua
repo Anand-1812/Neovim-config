cb28d99c156f96bd122fa88e0c9c1c800fb8929d
Anand-1812 <anand.work2005@gmail.com>
2024-12-13 09:48:00 +0530

    Add java server to the config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,7 +31,10 @@ return {
             lspconfig.pyright.setup({
                 capabilities = capabilities
             })
-    
+            lspconfig.jdtls.setup({
+                capabilities = capabilities
+            })
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})

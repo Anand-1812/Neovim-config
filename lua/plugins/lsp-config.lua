05e6941530d2fb85257825e874f9a37840269477
Anand-1812 <anand.work2005@gmail.com>
2025-06-24 10:16:23 +0530

    change mason to nvim-lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,66 +1,76 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	-- LSP Installer
+	{
+		"kabouzeid/nvim-lspinstall",
+		config = function()
+			require("lspinstall").setup()
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        automatic_installation = true,
-      })
-    end,
-  },
+			local lspconfig = require("lspconfig")
+			local cmp_caps = require("cmp_nvim_lsp").default_capabilities()
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local function setup_servers()
+				require("lspinstall").setup()
+				for _, srv in ipairs(require("lspinstall").installed_servers()) do
+					lspconfig[srv].setup({
+						capabilities = cmp_caps,
+						on_attach = function(client, bufnr)
+							local opts = { buffer = bufnr, noremap = true, silent = true }
+							vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+							vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, opts)
+							vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+							vim.keymap.set("n", "<leader>f", function()
+								vim.lsp.buf.format({ async = true })
+							end, opts)
+						end,
+					})
+				end
+			end
 
-      local servers = {
-        "lua_ls",
-        "clangd",
-        "tsserver",
-        "pyright",
-        "jdtls",
-        "solargraph",
-        "html",
-        "cssls",
-        "eslint",
-      }
+			setup_servers()
+			require("lspinstall").post_install_hook = function()
+				setup_servers()
+				vim.cmd("bufdo e")
+			end
+		end,
+	},
 
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-        })
-      end
+	-- Completion via nvim-cmp
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-      lspconfig.solargraph.setup({
-        cmd = { "solargraph", "stdio" },
-        filetypes = { "ruby" },
-        root_dir = lspconfig.util.root_pattern("Gemfile", ".git"),
-        settings = {
-          solargraph = {
-            diagnostics = true,
-          },
-        },
-        capabilities = capabilities,
-      })
-
-      local opts = { noremap = true, silent = true }
-
-      vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-      vim.keymap.set("n", "<leader>f", function()
-        vim.lsp.buf.format({ async = true })
-      end, opts)
-    end,
-  },
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+			})
+		end,
+	},
 }

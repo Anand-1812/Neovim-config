7b0b8a3ae98f632908cb88d608216cde12549425
Anand-1812 <anand.work2005@gmail.com>
2025-06-24 10:03:52 +0530

    fix: rainbow error, i don't know what that was

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,86 +1,112 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
 
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "nvim-treesitter/playground",
-    "windwp/nvim-ts-autotag",
-    "HiPhish/nvim-ts-rainbow2",
-  },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/playground",
+			"windwp/nvim-ts-autotag",
+		},
 
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash", "c", "cpp", "css", "gitcommit", "go", "html", "javascript",
-        "json", "lua", "markdown", "python", "ruby", "rust", "toml",
-        "typescript", "yaml"
-      },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash",
+					"c",
+					"cpp",
+					"css",
+					"gitcommit",
+					"go",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"python",
+					"ruby",
+					"rust",
+					"toml",
+					"typescript",
+					"yaml",
+				},
 
-      auto_install = true,
+				auto_install = true,
 
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
 
-      indent = {
-        enable = true,
-      },
+				indent = { enable = true },
 
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",
-          node_incremental = "<TAB>",
-          node_decremental = "<S-TAB>",
-          scope_incremental = "<C-space>",
-        },
-      },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<TAB>",
+						node_decremental = "<S-TAB>",
+						scope_incremental = "<C-space>",
+					},
+				},
 
-      autotag = {
-        enable = true,
-      },
+				autotag = { enable = true },
 
-      rainbow = {
-        enable = true,
-        query = "rainbow-parens",
-        strategy = require("ts-rainbow.strategy.global"),
-      },
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							["ab"] = "@block.outer",
+							["ib"] = "@block.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+					},
+				},
 
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-          },
-        },
-      },
+				playground = {
+					enable = true,
+					updatetime = 25,
+					persist_queries = false,
+				},
+			})
+		end,
+	},
 
-      playground = {
-        enable = true,
-        updatetime = 25,
-        persist_queries = false,
-      },
-    })
-  end,
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			local rainbow_delimiters = require("rainbow-delimiters")
+
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+					lua = rainbow_delimiters.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					html = "rainbow-tags",
+				},
+				priority = {
+					[""] = 110,
+					lua = 120,
+				},
+			}
+		end,
+	},
 }
-

0e7154af9b7bfb6007fb5235cdbbffe9599809ca
Anand-1812 <anand.work2005@gmail.com>
2025-06-24 09:24:21 +0530

    change the theme to moon

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,24 +1,23 @@
 return {
-  "folke/tokyonight.nvim",
+	"folke/tokyonight.nvim",
 
-  priority = 1000, -- Load before other plugins
+	priority = 1000, -- Load before other plugins
 
-  config = function()
-    require("tokyonight").setup({
-      style = "night",         -- Options: "storm", "moon", "night", "day"
-      transparent = false,
-      terminal_colors = true,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-      },
-      dim_inactive = true,
-      lualine_bold = true,
-    })
+	config = function()
+		require("tokyonight").setup({
+			style = "moon", -- Options: "storm", "moon", "night", "day"
+			transparent = false,
+			terminal_colors = true,
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = true },
+				functions = {},
+				variables = {},
+			},
+			dim_inactive = true,
+			lualine_bold = true,
+		})
 
-    vim.cmd.colorscheme("tokyonight")
-  end,
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
-
